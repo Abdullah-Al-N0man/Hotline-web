0a5312566c029d17d811ab17e1079038f9b441a9
Abdullah-Al-N0man <jesminakter0506@gmail.com>
2025-08-29 19:26:44 +0600

    script

---
+++ script.js
@@ -0,0 +1,59 @@
+// Counter
+let heartCounter = parseInt(document.getElementById("heart-count").innerText);
+let coinCounter = parseInt(document.getElementById("coin-count").innerText);
+let copyCounter = parseInt(document.getElementById("copy-count").innerText);
+
+// Functions
+document
+  .getElementById("card-container")
+  .addEventListener("click", function (event) {
+    const card = event.target.closest(".card");
+    // Call Btn
+    
+    if (event.target.innerText === "Call") {
+      const title = card.querySelector("h2").innerText;
+      const contact = card.querySelector("p").innerText;
+      const number = card.querySelector("h3").innerText;
+      if (coinCounter >= 20) {
+        alert(`Calling ${contact} ${number}...`);
+        coinCounter -= 20;
+        document.getElementById("coin-count").innerText = coinCounter;
+        const date = new Date().toLocaleTimeString();
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="history-item">
+            <div>
+              <div class="history-item-title">
+                ${title}
+              </div>
+              <div class="history-item-number">
+                ${number}
+              </div>
+            </div>
+            <div class="history-item-time">${date}</div>
+          </div>
+        `;
+        document.getElementById("history-card-container").appendChild(div);
+      } else {
+        alert("You do not have enough coins to make a call.");
+      }
+    }
+    // Copy Btn
+    else if (event.target.innerText === "Copy") {
+      console.log("Clicked Copy");
+      const number = card.querySelector("h3").innerText;
+      navigator.clipboard.writeText(number);
+      alert(`Number Copied: ${number}`);
+      copyCounter++;
+      document.getElementById("copy-count").innerText = copyCounter;
+    }
+    // Heart Btn
+    else if (event.target.className.includes("fa-heart")) {
+      heartCounter++;
+      document.getElementById("heart-count").innerText = heartCounter;
+    }
+  });
+
+document.getElementById("history-clear-btn").addEventListener('click', function(){
+  document.getElementById("history-card-container").innerHTML="";
+});
